2e1b6e3dd9de5121e0aec4c22efbfb31d4a01047
andrercastillon <andrercastillon@gmail.com>
2023-04-30 20:25:10 -0500

    compl

--- bankAccount.js
+++ bankAccount.js
@@ -8,35 +8,49 @@ class BankAccount {
 
 transactions = [];
 
-balance() //gets balance
-{
-  if(this.transactions.length == 0)
-  {
-    console.log("No History")
+balance() {
+
+  let sum = 0;
+
+  if (this.transactions.length == 0) {
+    console.log("No history")
   }
-  else if(this.transactions.length == 1)
-  {
-    this.transactions[0].amount
-  }
-  else
-  {
-    for (let index = 0; index < array.length; index++) {
-      const element = array[index];
-      
-    }
-  }
+  
+  else {
+      for (let i = 0; i < this.transactions.length; i++) {
+    sum += this.transactions[i].amount;
+ }
+ console.log(`Sum: ${sum}`)
+ return sum;
+ 
+}
 }
 
-deposit(amt)
-{
-  let currTransaction = new Transaction(amt, this.owner);
-  this.transactions.push(currTransaction)
+deposit(amt) {
+  let currTransaction = new Transaction(amt, this.owner)
+
+  if (amt > 0) {
+    this.transactions.push(currTransaction);
+    console.log("Deposit made", amt);
+  } 
+
+  else {
+    console.log("Unable to deposit negative amounts or no amount.")
+  }
 }
 
 charge(payee, amt)
 {
   let currTransaction = new Transaction(amt*-1, payee)
-  this.transactions.push(currTransaction)
+  
+  if (amt <= this.balance()) {
+    this.transactions.push(currTransaction);
+    console.log("Transaction approved", amt)
+  }
+
+  else {
+    console.log("Transaction declined", amt)
+  }
 }
 };
 
@@ -57,10 +71,10 @@ let myFirstBankAccount = new BankAccount("account123", "James Johnson");
 
 let myFirstTransaction = new Transaction(12.50, "Jil Joe");
 
-console.log(myFirstTransaction.amount);
+// console.log(myFirstTransaction.amount);
 
-myFirstBankAccount.deposit(10.50);
-myFirstBankAccount.deposit(100.50)
+myFirstBankAccount.deposit(100.50);
+myFirstBankAccount.charge(10.50, "Jil Joe")
 
 // console.log(myFirstBankAccount.transactions)
 myFirstBankAccount.balance();
